153025889c9e167bd57e570f2111ca3253fd7655
Wei Kin Huang <weikin.huang04@gmail.com>
2015-03-05 14:46:37 -0500

    Remove logging

--- git-npm-proxy/Program.cs
+++ git-npm-proxy/Program.cs
@@ -16,29 +16,21 @@ namespace git_npm_proxy
 
         static void Main(string[] args)
         {
-            w.WriteLine("88888888888888888888888888888888888888888888888");
-            //
-            // Setup the process with the ProcessStartInfo class.
-            //
             ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = @"C:\cygwin\bin\git.exe"; // Specify exe name.
+            start.FileName = @"C:\cygwin\bin\git.exe";
 
             Regex rmQuotes = new Regex("[\"]");
             List<string> argList = new List<string>();
-            // escape arguments, assume '"' is not allowed
-            w.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            // escape arguments, assume "'" is not allowed
             foreach (string arg in args)
             {
-                //Console.Error.WriteLine(arg);
                 string addArg = arg;
-                string[] addArgParts = new string[2];
-                string addArgPrefix = "";
                 if (addArg[0] == '-')
                 {
                     argList.Add(addArg);
                     continue;
                 }
-                if (Regex.Match(arg, "[\"]").Success)
+                if (Regex.Match(arg, "[']").Success)
                 {
                     addArg = rmQuotes.Replace(arg, "").ToString();
                 }
@@ -48,17 +40,11 @@ namespace git_npm_proxy
                 }
                 else
                 {
-                    w.WriteLine(ConvertToCygpath(addArg));
                     argList.Add("'" + ConvertToCygpath(addArg) + "'");
                 }
             }
-            w.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
             start.Arguments = String.Join(" ", argList.ToArray());
-            if (args[1] == "clone")
-            {
-                Console.Error.WriteLine(start.Arguments);
-            }
 
             // remove the current implementation of the proxy from the path, since we assume
             // that this proxy application is in a separate PATH than the actual git binary
@@ -79,8 +65,6 @@ namespace git_npm_proxy
             start.EnvironmentVariables["TEMP"] = Environment.GetEnvironmentVariable("TEMP");
             start.EnvironmentVariables["APPDATA"] = Environment.GetEnvironmentVariable("APPDATA");
 
-            w.WriteLine(Directory.GetCurrentDirectory());
-
             start.ErrorDialog = false;
             start.UseShellExecute = false;
             start.RedirectStandardOutput = true;
@@ -89,25 +73,26 @@ namespace git_npm_proxy
 
             Process process = Process.Start(start);
             process.WaitForExit();
-            Console.Write(process.StandardOutput.ReadToEnd().Trim());
-            Console.Error.Write(process.StandardError.ReadToEnd().Trim());
-            w.WriteLine("EXITING---------------------------------------");
-            Environment.Exit(0);
+            Console.Write(process.StandardOutput.ReadToEnd());
+            Console.Error.Write(process.StandardError.ReadToEnd());
+            Environment.Exit(process.ExitCode);
         }
 
         /// <summary>
-        /// Invokes 'cygpath' to convert a cygwin path to a windows path. 
+        /// Invokes 'cygpath' to convert a cygwin path to a unix path. 
         /// Note the cygpath executable must be in your PATH, or an exception will 
         /// silently occur and your original unconverted path will be returned
         /// </summary>
         /// <param name="path">The input path, potentially in cygwin format</param>
-        /// <returns>If the path does not contain the / character, it is simply returned.
+        /// <returns>If the path does not contain the \ character, it is simply returned.
         /// If it does, then cygpath is invoked, and the value returned from cygpath is returned.
         /// If an error occurs invoking cygpath, then the error is swallowed and the input path is simply returned.</returns>
         public static string ConvertToCygpath(string path)
         {
-            if (!path.Contains("\\"))
+            if (!path.Contains(@"\"))
+            {
                 return path;
+            }
 
             ProcessStartInfo startInfo = new ProcessStartInfo("cygpath", "-u '" + path.Replace("'", "\\'") + "'")
             {
@@ -119,14 +104,9 @@ namespace git_npm_proxy
 
             try
             {
-                w.WriteLine("CYGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-                var s = Process.Start(startInfo).StandardOutput.ReadToEnd().Trim();
-                w.WriteLine(path);
-                w.WriteLine(s);
-                w.WriteLine("CYGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-                return s;
+                return Process.Start(startInfo).StandardOutput.ReadToEnd().Trim();
             }
-            catch (Win32Exception) // file not found - can't find cygpath.exe. Don't care about being more specific with our error
+            catch (Win32Exception)
             { }
             return path;
         }
