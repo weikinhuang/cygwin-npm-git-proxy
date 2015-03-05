0d73111610ac1f977062b17fa8cdae8ba72a5b03
Wei Kin Huang <weikin.huang04@gmail.com>
2015-03-05 14:48:25 -0500

    More cleanup

--- git-npm-proxy/Program.cs
+++ git-npm-proxy/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace git_npm_proxy
 {
@@ -106,7 +102,7 @@ namespace git_npm_proxy
             {
                 return Process.Start(startInfo).StandardOutput.ReadToEnd().Trim();
             }
-            catch (Win32Exception)
+            catch (Exception)
             { }
             return path;
         }
